ad11625b5266265dcac2f8e7f2020b011dc67ed0
pablomzr <juantrane104@gmail.com>
2025-03-29 15:46:40 -0600

    Class Validator

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { EmployeesModule } from 'src/employees/employees.module';
 
 @Module({
+  imports: [EmployeesModule, ProductsModule],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
