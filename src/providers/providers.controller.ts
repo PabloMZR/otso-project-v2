e454f730ffbc9ff3283baffb78e2bc64ece76eb0
pablomzr <juantrane104@gmail.com>
2025-04-03 20:44:00 -0600

    Add Auth Decorator

--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -2,14 +2,10 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, U
 import { ProvidersService } from './providers.service';
 import { CreateProviderDto } from './dto/create-provider.dto';
 import { UpdateProviderDto } from './dto/update-provider.dto';
-import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { UserData } from 'src/auth/decorators/user.decorator';
 import { User } from 'src/auth/entities/user.entity';
-import { Roles } from 'src/auth/decorators/roles.decorator';
-import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 
-@UseGuards(AuthGuard)
-@UseGuards(RolesGuard)
 @Controller('providers')
 export class ProvidersController {
   constructor(private readonly providersService: ProvidersService) {}
@@ -19,9 +15,8 @@ export class ProvidersController {
     return this.providersService.create(createProviderDto);
   }
 
-   
-  @Roles(["Admin"])
-  
+
+  @Auth("Employee")
   @Get()
   findAll(@UserData() user: User) {
     if(user.userRoles.includes("Employee")) throw new UnauthorizedException("No estas Autorizado, SOLO ADMIN Y MANAGER")
