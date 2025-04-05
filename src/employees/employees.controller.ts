632516d88ca5c99f66eea7c1979197d90c921568
pablomzr <juantrane104@gmail.com>
2025-04-05 14:51:59 -0600

    Auth for routes

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -3,38 +3,41 @@ import { EmployeesService } from './employees.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
 import { FileInterceptor } from '@nestjs/platform-express/multer';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ROLES } from 'src/auth/constants/roles.constans';
 
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
 
+  @Auth(ROLES.MANAGER)
   @Post()
   create(@Body() createEmployeeDto: CreateEmployeeDto) {
     return this.employeesService.create(createEmployeeDto);
   }
-
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Post('Upload')
   @UseInterceptors(FileInterceptor('file'))
   uploadPhoto(@UploadedFile() file: Express.Multer.File){
     console.log(file);
     return "ok"
   }
-
+  @Auth(ROLES.MANAGER)
   @Get()
   findAll() {
     return this.employeesService.findAll();
   }
-
+  @Auth(ROLES.MANAGER)
   @Get(':id', )
   findOne(@Param('id', new ParseUUIDPipe({version: '4'})) id: string) {
     return this.employeesService.findOne(id);
   }
-
+  @Auth(ROLES.MANAGER)
   @Patch(':id')
   update(@Param('id',new ParseUUIDPipe({version: '4'})) id: string, @Body() updateEmployeeDto: UpdateEmployeeDto) {
     return this.employeesService.update(id, updateEmployeeDto);
   }
-
+  @Auth(ROLES.MANAGER)
   @Delete(':id')
   remove(@Param('id',new ParseUUIDPipe({version: '4'})) id: string) {
     return this.employeesService.remove(id);
--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -5,41 +5,43 @@ import { UpdateProviderDto } from './dto/update-provider.dto';
 import { UserData } from 'src/auth/decorators/user.decorator';
 import { User } from 'src/auth/entities/user.entity';
 import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ROLES } from 'src/auth/constants/roles.constans';
 
 @Controller('providers')
 export class ProvidersController {
   constructor(private readonly providersService: ProvidersService) {}
 
+  @Auth(ROLES.MANAGER)
   @Post()
   create(@Body() createProviderDto: CreateProviderDto) {
     return this.providersService.create(createProviderDto);
   }
 
 
-  @Auth("Employee")
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Get()
   findAll(@UserData() user: User) {
     if(user.userRoles.includes("Employee")) throw new UnauthorizedException("No estas Autorizado, SOLO ADMIN Y MANAGER")
     return this.providersService.findAll();
   }
-   
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Get('/name/:name') 
   findByName(@Param('name') name: string) {
     return this.providersService.findOneByName(name);
   }
-
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Get(':id')
   findOne(@Param('id') id: string) {
     const provider =  this.providersService.findOne(id);
     if(!provider) throw new NotFoundException()
       return provider
   }
-
+  @Auth(ROLES.MANAGER)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProviderDto: UpdateProviderDto) {
     return this.providersService.update(id, updateProviderDto);
   }
-
+  @Auth(ROLES.MANAGER)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.providersService.remove(id);
